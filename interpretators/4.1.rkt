9e694af8c8a247887631c59e38855edc6cca6a1d
RomanFadeev <r.fadeew1991@yandex.ru>
2021-03-15 11:04:27 +0300

    добавляет новые фичи для интерпретатора

--- interpretators/4.1.rkt
+++ interpretators/4.1.rkt
@@ -1,13 +1,12 @@
 #lang sicp
 
-
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
-        ((quoted? exp) (text-of-quatation exp))
+        ((quoted? exp) (text-of-quotation exp))
         ((assignment? exp) (eval-assignment exp env))
         ((definition? exp) (eval-definition exp env))
-        ((if? exp) (eval-if exp-env))
+        ((if? exp) (eval-if exp env))
         ((lambda? exp)
          (make-procedure (lambda-parameters exp)
                          (lambda-body exp)
@@ -15,8 +14,249 @@
         ((begin? exp)
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (eval (cond->if exp) env))
+        ((and? exp) (eval (and->if exp) env))
         ((application? exp)
          (apply (eval (operator exp) env)
                 (list-of-values (operands exp) env)))
         (else
          (error "Неизвестный тип выражения -- EVAL" exp))))
+
+(define (eval2 exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((get (operator exp)) ((get (operator exp)) operands exp env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Неизвестный тип выражения -- EVAL" exp))))
+
+(define (install-eval-package)
+  (define (quoted exp env) (text-of-quotation exp env))
+  (define (assignment exp env) (eval-assignment exp env))
+  (define (definition exp env) (eval-definition exp env))
+  (define (if exp env) (eval-if exp env))
+  (define (lambda exp env) (make-procedure (lambda-parameters exp)
+                                           (lambda-body exp)
+                                           env))
+  (define (begin exp env) (eval-sequence (begin-actions exp) env))
+  (define (cond exp env))
+
+  (put 'quote quoted)
+  (put 'set! assignment)
+  (put 'define definition)
+  (put 'if if)
+  (put 'lambda lambda)
+  (put 'cond cond)
+  )
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           arguments
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Неизвестный тип процедуры -- APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternate exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
+(define (val-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-variable!
+    (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'ok)
+
+(define (list-of-values-left exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((fst-value (eval (first-operand exps) env)))
+        (cons fst-value
+              (list-of-values (rest-operands exps) env)))
+      ))
+
+
+(define (list-of-values-right exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((rest (list-of-values (rest-operands exps) env)))
+        (cons (eval (first-operand exps) env)
+              rest))
+      ))
+
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+(define (variable? exp)
+  (symbol? exp))
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caddr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdard exp)
+                   (cddr exp))))
+
+(define (lambda? exp)
+  (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp)
+  (cadr exp))
+
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predivate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternate exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (begin? exp) (tagged-list? exp 'begin))
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+(define (first-operands ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if? exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false ; нет ветви else
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "Ветвь ELSE не последняя -- COND-IF"
+                       clauses))
+            (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest))))))
+
+
+(define (and->if exp)
+  (expand-and-clauses (and-clauses exp)))
+
+(define (expand-and-clauses clauses)
+  (if (last-clause? clauses)
+      (make-if (car clauses)
+               (car clauses)
+               'false
+               )
+      (make-if (car clauses)
+               (expand-and-clauses (cdr clauses))
+               'false)))
+
+(define (and? exp) (tagged-list? exp 'and))
+(define (and-clauses exp) (cdr exp))
+(define (last-clause-and? exp) (null? (cdr exp)))
+
+
+(define (or->if exp)
+  (expand-and-clauses (or-clauses exp)))
+
+(define (expand-or-clauses clauses)
+  (if (last-clause? clauses)
+      (make-if (car clauses)
+               (car clauses)
+               'false
+               )
+      (make-if (car clauses)
+               (car clauses)
+               (expand-and-clauses (cdr clauses))
+               )))
+
+(define (or? exp) (tagged-list? exp 'or))
+(define (or-clauses exp) (cdr exp))
+(define (last-clause-or? exp) (null? (cdr exp)))
